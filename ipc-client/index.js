5f4c9e4e089ead333d476365ef7727c6878bd338
Bill Kidwell <kidwell.bill@gmail.com>
2018-10-30 21:11:37 -0400

    Stream a file to a buffer
    The client will pipe the contents of the file on the first message from
    the server.
    
    The server will store the contents in a streamBuffer.
    On end, the server reports the buffer size.

--- ipc-client/index.js
+++ ipc-client/index.js
@@ -2,18 +2,24 @@
 
 const xpipe = require('xpipe');
 const net = require('net');
+const fs = require('fs');
 
 let ipcPath = xpipe.eq('/tmp/my.sock');
 const client = net.connect(ipcPath, () => {
-    console.log('client connected');
-    client.write('world!\r\n');
+    console.log('client connected');    
 });
+
+var file = null;
 
 client.on('data', (data) => {
     console.log(data.toString());
-    client.end();
+
+    console.log('loading file');
+    file = fs.createReadStream('./testfile.exe');
+    file.pipe(client);
 });
 
 client.on('end', () => {
+    file.close();
     console.log('client disconnected');
 })
--- ipc-server/index.js
+++ ipc-server/index.js
@@ -3,15 +3,29 @@
 const xpipe = require('xpipe');
 const net = require('net');
 let ipcPath = xpipe.eq('/tmp/my.sock');
+const fs = require('fs');
+const streamBuffers = require('stream-buffers');
 
 const server = net.createServer( { allowHalfOpen: false }, (c) => { // connection listener
+
     console.log('server connected');
+
+    // Create a stream buffer
+    var streamBuffer = new streamBuffers.WritableStreamBuffer({
+        initialSize: (100 * 1024),  // Starts at 100k
+        incrementAmount: (10 * 1024), // Grows by 10k
+    });
+
+    c.write('send\r\n')
+    // client will pipe over file contents
+    c.pipe(streamBuffer);
+
+    // When the connection ends, we can see the streambuffer contents.
     c.on('end', () => {
         console.log('server disconnected');        
-    })
+        console.log('Stream buffer is ' + streamBuffer.size());
+    });
 
-    c.write('hello\r\n');
-    c.pipe(c);
 });
 
 server.listen(ipcPath, () => {
